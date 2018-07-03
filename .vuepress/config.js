2e9e66e23eeb1971aeeb57d54a7784fb8aed3956
hisey <shijianxiang@mmtvip.com>
2018-07-03 08:54:00 +0800

    bug

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'hisey个人站点',
     description: '算是闲谈吧',
-    base:'/hisey/front-end-doc/docs/',
+    base:'front-end-doc/doc/',
     port:4864,
     dest:"./doc",
     themeConfig: {
