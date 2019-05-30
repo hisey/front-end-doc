6a8c7b8639fdd53d47f690e126787fc89261fc7b
hisey <2929736912@qq.com>
2019-05-30 10:37:33 +0800

    优化

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,82 +1,95 @@
 module.exports = {
-  title: '木林',
-  description: '个人站点',
-  base: '/front-end-doc/',
+  title: "hisey",
+  description: "不一样的世界，等你去发现",
+  base: "/front-end-doc/",
   port: 4864,
   dest: "./docs",
   themeConfig: {
-    nav: [{
-      text: '首页',
-      link: '/'
+    lastUpdated: "Last Updated",
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, 默认值是 undefined.
     },
-    {
-      text: "Git指南",
-      items: [{
-        text: 'Git工作流',
-        link: '/src/base/git/flow'
+    nav: [
+      {
+        text: "首页",
+        link: "/"
       },
       {
-        text: '常用命令行',
-        link: '/src/base/git/bash'
-      },
-      ]
-    },
-    {
-      text: '基础知识',
-      items: [{
-        text: 'es6代码风格',
-        link: '/src/base/codeStyle/'
-      },
-      {
-        text: 'css干货',
-        link: '/src/base/cssDoc/'
+        text: "Git指南",
+        items: [
+          {
+            text: "Git工作流",
+            link: "/src/base/git/flow"
+          },
+          {
+            text: "常用命令行",
+            link: "/src/base/git/bash"
+          }
+        ]
       },
       {
-        text: 'js干货',
-        link: '/src/base/jsDoc/'
+        text: "基础知识",
+        items: [
+          {
+            text: "es6代码风格",
+            link: "/src/base/codeStyle/"
+          },
+          {
+            text: "css干货",
+            link: "/src/base/cssDoc/"
+          },
+          {
+            text: "js干货",
+            link: "/src/base/jsDoc/"
+          },
+          {
+            text: "弹性盒子布局",
+            link: "/src/base/flexbox/"
+          }
+        ]
       },
       {
-        text: '弹性盒子布局',
-        link: '/src/base/flexbox/'
-      },
-      ]
-    },
-    {
-      text: "解决方案",
-      items: [{
-        text: '前后端分离',
-        link: '/src/case/webSplit/'
+        text: "解决方案",
+        items: [
+          {
+            text: "前后端分离",
+            link: "/src/case/webSplit/"
+          },
+          {
+            text: "代码部署",
+            link: "/src/case/autoDeploy/"
+          }
+        ]
       },
       {
-        text: '代码部署',
-        link: '/src/case/autoDeploy/'
+        text: "小程序",
+        items: [
+          {
+            text: "mpvue搭建小程序",
+            link: "/src/smallProgram/mpvue/"
+          }
+        ]
       },
-      ]
-    },
-    {
-      text: '小程序',
-      items: [{
-        text: 'mpvue搭建小程序',
-        link: '/src/smallProgram/mpvue/'
-      },],
-    },
-    {
-      text: "vue",
-      items: [
-        {
-          text: '基础',
-          link: '/src/vue/'
-        },
-        {
-          text: 'vue组件示例',
-          link: '/src/vue/vueCompont/'
-        },]
-    }, {
-      text: "关于我", link: "/src/aboutMe/"
-    }
+      {
+        text: "vue",
+        items: [
+          {
+            text: "基础",
+            link: "/src/vue/"
+          },
+          {
+            text: "vue组件示例",
+            link: "/src/vue/vueCompont/"
+          }
+        ]
+      },
+      {
+        text: "关于我",
+        link: "/src/aboutMe/"
+      }
     ],
-    sidebar: 'auto',
-    repo: '/hisey/front-end-doc',
-    repoLabel: 'gitHub',
+    sidebar: "auto",
+    repo: "/hisey/front-end-doc",
+    repoLabel: "gitHub"
   }
-}
+};
--- README.md
+++ README.md
@@ -12,3 +12,28 @@ features:
   details: node
 footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
+### 如何编辑此文档，像数 1, 2, 3 一样容易
+
+```bash
+# 获取代码
+# git clone https://github.com/hisey/front-end-doc.git
+
+# 安装 vuepress 
+npm install -g vuepress
+
+# 开始写作
+vuepress dev 
+
+
+# 构建静态文件
+vuepress build  
+
+# 构建完成之后，将代码推送到 https://github.com/hisey/front-end-doc.git 即可，
+# 访问 https://hisey.github.io/front-end-doc/可查看
+```
+
+::: warning 注意
+请确保你的 Node.js 版本 >= 8。
+:::
+
+
